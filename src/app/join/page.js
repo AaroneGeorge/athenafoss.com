7312bf16647021894e03d1bcaf1efc32961e2828
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-10-04 22:10:51 +0530

    button changes

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -3,10 +3,18 @@ import ApplyButton from "../common/ApplyButton/ApplyButton";
 export default function Join() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <ApplyButton 
+      {/* <ApplyButton 
         redirectUrl="https://www.google.com" 
         newTab={true} 
-      />
+      /> */}
+      <a
+        className="bg-gray-800 px-5 py-2.5 rounded-md text-sm hover:bg-black/80 transition-all ease-in"
+        href="https://www.google.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Apply Now
+      </a>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl tracking-tighter opacity-90 font-medium  text-gray-800">
-        Within silence and patience true strength quietly emerges unstoppable
+        Persistence is very important. You should not give up unless you are
+        forced to give up.
       </h1>
     </div>
   );
