0980e7d15f4976f98c889a4549364bcd9c7da751
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-10-05 13:05:32 +0530

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import React from "react";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl tracking-tighter opacity-90 font-medium  text-gray-800">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl tracking-tighter opacity-90 font-medium text-gray-800 text-center whitespace-normal lg:whitespace-nowrap overflow-hidden">
         Persistence is very important. You should not give up unless you are
         forced to give up.
       </h1>
