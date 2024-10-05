cd3d0a7e84725633b167db2b6ad84ec25b1d4c9f
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-10-05 11:27:25 +0530

    removed common

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -1,12 +1,6 @@
-import ApplyButton from "../common/ApplyButton/ApplyButton";
-
 export default function Join() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      {/* <ApplyButton 
-        redirectUrl="https://www.google.com" 
-        newTab={true} 
-      /> */}
       <a
         className="bg-gray-800 px-5 py-2.5 rounded-md text-sm hover:bg-black/80 transition-all ease-in"
         href="https://www.google.com"
