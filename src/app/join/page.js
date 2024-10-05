15e3505fb1b991dce03aa89c45fb411f12bcf1a1
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-10-05 13:44:30 +0530

    added apply form link

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -3,7 +3,7 @@ export default function Join() {
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <a
         className="bg-gray-800 px-5 py-2.5 rounded-md text-sm hover:bg-black/80 transition-all ease-in"
-        href="https://www.google.com"
+        href="https://forms.gle/SBDoxrkJsYG8maEK8"
         rel="noopener noreferrer"
       >
         Apply Now
