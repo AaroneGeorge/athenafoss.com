61ce5bb4d871fdbfbeac50b551b9e0658ff7ebf8
AaroneGeorge <aaroneofficial1879@gmail.com>
2025-01-13 17:14:43 +0530

    redirect url changed

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -3,7 +3,7 @@ export default function Join() {
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <a
         className="bg-gray-800 px-5 py-2.5 rounded-md text-sm hover:bg-black/80 transition-all ease-in"
-        href="https://forms.gle/SBDoxrkJsYG8maEK8"
+        href="https://cosmic-tractor-712.notion.site/179c4091f4dc802495bcd3510df919ad"
         rel="noopener noreferrer"
       >
         Apply Now
